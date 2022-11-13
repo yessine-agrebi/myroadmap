7e1c7947ac2e2e60d75353be7ee25b4e0d8876bf
ragnar-codes <yessine.agrebi.aiesec@gmail.com>
2022-11-13 15:16:40 +0100

    fixing deployment

--- src/components/Frontend.jsx
+++ src/components/Frontend.jsx
@@ -3,7 +3,7 @@ import Card from './Card';
 import {frontend} from '../data/frontend';
 const Frontend = () => {
   return (
-    <div className='flex flex-col items-center justify-center gap-5'>
+    <div className='flex flex-col items-center justify-center gap-5 sm:mx-auto'>
       {frontend ? frontend.map((course, index) => {
           return (
             
