a18a0ab1c450f4f23d4a3d8982e21d4bc8bf6dc2
ragnar-codes <yessine.agrebi.aiesec@gmail.com>
2022-11-13 21:18:10 +0100

    add google anlytics

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import React, {useEffect} from 'react';
 import Frontend from './components/Frontend';
 import Backend from './components/Backend';
 import NavBar from './components/NavBar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Hero from './components/Hero';
 import Footer from './components/Footer';
+import ReactGA from 'react-ga';
+
+const TRACKING_ID = "G-PEHJ803RZ5";
+ReactGA.initialize(TRACKING_ID);
 
 
 function App() {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
   return (
     <div>
       <Router>
